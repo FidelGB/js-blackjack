b527c5f9953442dd81d68b3c576b2915904087c2
Fidel GB <fidelgarcia.soft@gmail.com>
2021-05-29 12:17:25 -0500

    Added function to get card

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,8 @@
     let deck = [];
     const values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
     const types = ["C","D","H","S"]
+
+    // Funcion para crear nuevo deck
     const crearDeck = () => {
         for (const value of values) {
             for (const type of types) {
@@ -18,5 +20,23 @@
         deck = _.shuffle(deck);
     }
 
+    // Funcion que permite tomar una carta
+    const pedirCarta = () => {
+        if(deck.length !== 0){
+            return deck.pop();
+        }
+        throw "No hay cartas en el deck";
+    }
+    
+    const valorCarta = (carta) => {
+        const valor = carta.substring(0, carta.length - 1);
+        return (isNaN(valor)) ? 
+                    (valor === "A" ? 11 : 10) :
+                valor * 1;
+    }
+    
     crearDeck();
+    const carta = pedirCarta();
+    const valor = valorCarta(carta);
+    console.log({valor});
 })()
